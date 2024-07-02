efcdc1f5160f2ef87f2fdad56d809ac1aa2e969c
Rahib Kibria <107937029+Rahib50@users.noreply.github.com>
2024-07-02 21:38:28 +0600

    Task8 fixed

--- 3/Task8.java
+++ 3/Task8.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Task8 {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
-          int arr[] = {7,13,2,10,6};
+          int[] arr = new int[5];
+          for(int idx = 0; idx < 5; idx++){
+               arr[idx] = sc.nextInt();
+          }
           
           int min = 0; int max = 0; int min_idx = 0; int max_idx = 0;
           for(int i = 0; i < 5; i++){
@@ -25,32 +28,8 @@ public class Task8 {
                }
 
           }
-          System.out.println("The number largest " +max + " was found at location " + max_idx);
-          System.out.println("The number smallest " + min + " was found at location " + min_idx);
-
-
-          int arr2[] = {2, 4, -5, 12, 3};
-          min = 0; max = 0; min_idx = 0; max_idx = 0;
-          for(int i = 0; i < 5; i++){
-               if(i == 0){
-                    min = arr2[i];
-                    min_idx = i;
-               }
-               
-               else{
-                    if(min > arr2[i]){
-                         min = arr2[i];
-                         min_idx = i;
-                    }
-                    if(max < arr2[i]){
-                         max = arr2[i];
-                         max_idx = i; 
-                    }
-               }
-
-          }
-          System.out.println("The number largest " +max + " was found at location " + max_idx);
-          System.out.println("The number smallest " + min + " was found at location " + min_idx);
+          System.out.println("The largest number " +max + " was found at location " + max_idx);
+          System.out.println("The smallest number " + min + " was found at location " + min_idx);
 
           sc.close();
      }
